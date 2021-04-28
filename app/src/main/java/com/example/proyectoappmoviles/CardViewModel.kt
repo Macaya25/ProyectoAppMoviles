dfb7753686fdd26d012f956af5772d134b0b8b00
Macaya25 <afmacaya@miuandes.cl>
2021-04-27 23:15:50 -0400

    Added cup icon to deck.

--- app/src/main/java/com/example/proyectoappmoviles/CardViewModel.kt
+++ app/src/main/java/com/example/proyectoappmoviles/CardViewModel.kt
@@ -13,7 +13,7 @@ class CardViewModel(application: Application) : AndroidViewModel(application) {
         list.clear()
         val mutableCards = deck.toMutableList()
         mutableCards.add("?")
-        mutableCards.add("taza")
+        mutableCards.add(9749.toChar().toString())
         val cards = mutableCards.chunked(3)
         for(i in cards.indices){
             list.add(CardItem(cards[i].size, cards[i]))
